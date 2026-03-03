8b59a35031d4c920a2e678e52826b5c1b37311ce
student802-altkom <student802@altkomlabs.onmicrosoft.com>
2026-03-03 11:24:32 +0100

    Add versioning information to greet function

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-def greet(name):
-    return f"Hello, {name}!"
-if __name__ == "__main__":
-    print(greet("Student"))
 from datetime import date
+
 def greet(name):
+
     return f"Hello, {name}!"
+
 def get_version():
+
     return "Version: 1.0.0 | Build date: 2026-03-02"
+
 if __name__ == "__main__":
+
     print(get_version())
+
     print(greet("Student"))
+
